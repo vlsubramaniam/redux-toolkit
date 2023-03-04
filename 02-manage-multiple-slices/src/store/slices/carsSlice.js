dcae8fd973626d65b743ef92819b4c8e7b182c63
vlsubramaniam <subramaniam.vls@gmail.com>
2023-03-05 01:27:44 +0530

    05-MAR-2023

--- 02-manage-multiple-slices/src/store/slices/carsSlice.js
+++ 02-manage-multiple-slices/src/store/slices/carsSlice.js
@@ -4,22 +4,22 @@ const carsSlice = createSlice({
     name: 'cars',
     initialState: {
         searchTerm: '',
-        cars: []
+        data: []
     },
     reducers: {
         changeSearchTerm(state, action) {
             state.searchTerm = action.payload
         },
         addCar(state, action) {
-            state.cars.push({
+            state.data.push({
                 name: action.payload.name,
                 cost: action.payload.cost,
                 id: nanoid()
             })
         },
         removeCar(state, action) {
-            const updated = state.cars.filter(car => car.id !== action.payload);
-            state.cars = updated;
+            const updated = state.data.filter(car => car.id !== action.payload);
+            state.data = updated;
         }
     }
 })
--- 02-manage-multiple-slices/src/store/slices/formSlice.js
+++ 02-manage-multiple-slices/src/store/slices/formSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { addCar } from "./carsSlice";
 
 
 const formSlice = createSlice({
@@ -15,6 +16,12 @@ const formSlice = createSlice({
         changeCost(state, action) {
             state.cost = action.payload;
         }
+    },
+    extraReducers(builder) {
+        builder.addCase(addCar, (state, action) => {
+            state.name = '';
+            state.cost = 0;
+        })
     }
 })
 
