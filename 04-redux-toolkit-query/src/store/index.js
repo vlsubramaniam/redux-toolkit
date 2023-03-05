307a8c055b97d8446e46ce583582b368ddf5a18b
vlsubramaniam <subramaniam.vls@gmail.com>
2023-03-05 23:54:30 +0530

    05-MAR-2023

--- 04-redux-toolkit-query/src/store/index.js
+++ 04-redux-toolkit-query/src/store/index.js
@@ -1,12 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+
 import { usersReducer } from './slices/usersSlice';
+import { albumsApi, useFetchAlbumsQuery } from './apis/albumsApi';
+
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
+    [albumsApi.reducerPath]: albumsApi.reducer
   },
+  middleware: (getDefaultMiddlware) => {
+    return getDefaultMiddlware()
+      .concat(albumsApi.middleware)
+  }
 });
+
+setupListeners(store.dispatch);
 
 export * from './thunks/fetchUsers';
 export * from './thunks/addUser';
 export * from './thunks/removeUser';
+
+export { useFetchAlbumsQuery };
