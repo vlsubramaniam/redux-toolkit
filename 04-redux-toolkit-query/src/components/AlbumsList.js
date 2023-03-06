8c5d946f7648584da7059f1d8b7c1aac3f2ffd5a
vlsubramaniam <subramaniam.vls@gmail.com>
2023-03-06 21:23:43 +0530

    06-MAR-2023

--- 04-redux-toolkit-query/src/components/AlbumsList.js
+++ 04-redux-toolkit-query/src/components/AlbumsList.js
@@ -1,10 +1,38 @@
-import { useFetchAlbumsQuery } from "../store";
+import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
+import Skeleton from './Skeleton';
+import ExpandablePanel from './ExpandablePanel';
+import Button from './Button';
 
 
 function AlbumsList({ user }) {
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
-  console.log(data, error, isLoading)
-  return <div>Albums for {user.name}</div>;
+  const [addAlbum, results] = useAddAlbumMutation();
+  let content;
+
+  const handleAddAlbum = () => addAlbum(user)
+
+  if (isLoading) {
+    content = <Skeleton times={3} />
+  } else if (error) {
+    content = <div>Error loading albums</div>
+  } else {
+    content = data.map(album => {
+      const header = <div>{album.title}</div>
+      return <ExpandablePanel key={album.id} header={header}>
+        List of photos
+      </ExpandablePanel>
+    })
+  }
+
+  return (
+    <div>
+      <div>Albums for {user.name}</div>
+      <Button onClick={handleAddAlbum}>
+        + Add Album
+      </Button>
+      <div> {content} </div>
+    </div>
+  )
 }
 
 export default AlbumsList;
--- 04-redux-toolkit-query/src/store/index.js
+++ 04-redux-toolkit-query/src/store/index.js
@@ -2,14 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 
 import { usersReducer } from './slices/usersSlice';
-import { albumsApi, useFetchAlbumsQuery } from './apis/albumsApi';
+import { albumsApi, useFetchAlbumsQuery, useAddAlbumMutation } from './apis/albumsApi';
 
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
     [albumsApi.reducerPath]: albumsApi.reducer
-  },
+  }, // redux thunk
   middleware: (getDefaultMiddlware) => {
     return getDefaultMiddlware()
       .concat(albumsApi.middleware)
@@ -18,8 +18,11 @@ export const store = configureStore({
 
 setupListeners(store.dispatch);
 
+// Async Thunks
 export * from './thunks/fetchUsers';
 export * from './thunks/addUser';
 export * from './thunks/removeUser';
 
-export { useFetchAlbumsQuery };
+
+// Redux Toolkit Query
+export { useFetchAlbumsQuery, useAddAlbumMutation };
