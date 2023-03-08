c1de77076e6141d35579d3ef254de84b0c32bc90
vlsubramaniam <subramaniam.vls@gmail.com>
2023-03-09 00:09:44 +0530

    09-MAR-2023

--- 04-redux-toolkit-query/src/components/PhotosList.js
+++ 04-redux-toolkit-query/src/components/PhotosList.js
@@ -1,11 +1,21 @@
 import PhotosListItem from "./PhotosListItem";
-import { useFetchPhotosQuery } from "../store";
+import { useFetchPhotosQuery, useAddPhotoMutation } from "../store";
+import Button from "./Button";
 
-function PhotosList({album}) {
-    useFetchPhotosQuery(album)
-    return(<>
-        Photos List
-    </>)
+function PhotosList({ album }) {
+    useFetchPhotosQuery(album);
+    const [addPhoto, addPhotoResults] = useAddPhotoMutation();
+
+    const handleAddPhoto = () => addPhoto(album);
+
+    return (<div>
+        <div className='m-2 flex flex-row items-center justify-between'>
+            <h3 className="text-lg font-bold">Photos in {album.title}</h3>
+            <Button loading={addPhotoResults.isLoading} onClick={handleAddPhoto}>
+                + Add Photo
+            </Button>
+        </div>
+    </div>)
 }
 
 
